645c23bd1dac3917109188f48d25910d76deecb1
Thomas Depke <tommydepke@gmail.com>
2022-07-17 02:45:05 -0600

    Many new additions and bug fixes.
    * Automatic variables ($@, $?, and $^) can now be used in targets.
    * Variables are expanded in the 'target: dependencies' definition line.
    * Multiple targets can be defined in the aforementioned line.
    * New .PHONY specifier can be used to have a target disregard any file
      with the same name.
    * Fixed bug: multiple targets with same name causing problems.
    * Fixed bug: $info macro not suppressing output.
    * Fixed bug: $shell macro not converting newlines to spaces.
    * Fixed bug: if a target would run more than once, cyclic dependency was
      incorrectly reported.

--- bake.lua
+++ bake.lua
@@ -1,6 +1,8 @@
 local fs = require("filesystem")
 local shell = require("shell")
 
+local xprint = require("xprint")
+
 ---@type table<string, any>
 local args = {}
 do
@@ -43,7 +45,6 @@ do
 end
 
 ---@class MakeTarget
----@field name string
 ---@field update_time number
 ---@field dependencies table<integer, string>
 ---@field commands table<integer, string>
@@ -51,6 +52,20 @@ end
 local macros = {}
 ---@type table<string, MakeTarget>
 local targets = {}
+---@type table<integer, string>
+local targets_ordering = {}
+
+--- Metadata for targets (such as .PHONY). This is kept separate from the
+--- targets table to allow metadata to be defined independently (even for
+--- targets that don't exist).
+---@type table<string, table>
+local targets_metadata = setmetatable({}, {
+    __index = function(t, k)
+        local new_entry = {}
+        t[k] = new_entry
+        return new_entry
+    end
+})
 
 --- Resolve macros and commands
 ---@param text string
@@ -73,11 +88,14 @@ local function resolve_macros(text)
                     return nil
                 end,
                 info = function()
-                    return "echo '" .. args .. "'"
+                    return "@echo '" .. args .. "'"
                 end,
                 shell = function()
                     local stream = io.popen(args)
                     local result = stream:read("a")
+                    if result then
+                        result = result:gsub("\n", " ")
+                    end
                     stream:close()
                     return result
                 end,
@@ -100,24 +118,51 @@ local function resolve_macros(text)
 end
 
 do
-    ---@type integer
-    local current_target = nil
+    ---@type string
+    local current_targets = nil
+    ---@type string
+    local current_dependencies = nil
+    ---@type table<integer, string>
+    local last_target_commands = {}
+
+    --- Builds a new target using data stored in current_targets,
+    --- current_dependencies and last_target_commands
+    local function add_targets()
+        for target_name in current_targets:gmatch("%S+") do
+            if targets[target_name] then
+                io.stderr:write("bake: Found multiple definitions for target \'" .. target_name .. "\'.\n")
+                os.exit(1)
+            end
+            local new_target = {
+                update_time = -1,
+                dependencies = {},
+                commands = {}
+            }
+            targets[target_name] = new_target
+            targets_ordering[#targets_ordering + 1] = target_name
+
+            for dependency in current_dependencies:gmatch("%S+") do
+                table.insert(new_target.dependencies, dependency)
+            end
+            for i, command in ipairs(last_target_commands) do
+                new_target.commands[i] = command
+            end
+        end
+    end
+
+    -- Iterate each line in file (skipping any that begin with '#' character or
+    -- only whitespace). Check line for macros, target definitions, or commands
     for line in bakefile_content:gmatch("[^\r\n]+") do
         if not line:match("^#") and line:match("%S") then
-            local target_name, dependencies = line:match("^([^%s:]+):(.*)")
-            if target_name then
-                current_target = #targets + 1
-                targets[current_target] = {
-                    name = target_name,
-                    update_time = -1,
-                    dependencies = {},
-                    commands = {}
-                }
-                
-                for dependency in dependencies:gmatch("%S+") do
-                    table.insert(targets[current_target].dependencies, dependency)
+            if line:find("%s") ~= 1 then
+                if current_targets then
+                    add_targets()
+                    current_targets = nil
+                    current_dependencies = nil
+                    last_target_commands = {}
                 end
-            else
+                line = resolve_macros(line)
+
                 -- TODO: Optimize pattern matching
                 local macro_name, macro_value = line:match("^([^=]+)=(.*)")
                 if macro_name then
@@ -125,28 +170,32 @@ do
                     macro_name = macro_name:match("^%s*(.-)%s*$")
                     macro_value = macro_value:match("^%s*(.-)%s*$")
 
-                    macros[macro_name] = resolve_macros(macro_value)
-                elseif current_target and line:match("^%s") then
-                    table.insert(targets[current_target].commands, line:match("^%s*(.*)"))
+                    macros[macro_name] = macro_value
                 else
-                    current_target = nil
+                    local target_names, dependencies = line:match("^([^:]+):(.*)")
+
+                    if target_names then
+                        if target_names:match("^%.PHONY%s*$") then
+                            for target_name in dependencies:gmatch("%S+") do
+                                targets_metadata[target_name].phony = true
+                            end
+                        else
+                            current_targets = target_names
+                            current_dependencies = dependencies
+                        end
+                    end
                 end
+            elseif current_targets then
+                table.insert(last_target_commands, line:match("^%s*(.*)"))
             end
         end
     end
-end
-
---- Get a target by name
----@param name string
----@return MakeTarget
-local function get_target(name)
-    for _, target in pairs(targets) do
-        if target.name == name then
-            return target
-        end
+    if current_targets then
+        add_targets()
     end
-    io.stderr:write("bake: Target not found: " .. name .. "\n")
-    os.exit(1)
+
+    print("finished file scan")
+    xprint({}, "macros", macros, "targets", targets, "targets_metadata", targets_metadata)
 end
 
 --- Run a command
@@ -166,61 +215,92 @@ end
 ---@type table<MakeTarget, boolean>
 local traversed_targets = {}
 
---- Run a target
----@param target MakeTarget
+--- Run a target. Returns the file modification time if applicable, or math.huge
+--- to indicate that the target updated.
+---@param target_name string
 ---@param is_first boolean
 ---@return number
-local function run_target(target, is_first)
+local function run_target(target_name, is_first)
+    -- Find the target from the name (it may not be found, or it could be an
+    -- existing file)
+    local target = targets[target_name]
+    local target_path = shell.resolve(target_name)
+    if not target then
+        if not fs.exists(target_path) and not targets_metadata[target_name].phony then
+            io.stderr:write("bake: Target not found: " .. target_name .. "\n")
+            os.exit(1)
+        elseif is_first then
+            print("bake: Nothing to be done for \'" .. target_name .. "\'.")
+        end
+        if targets_metadata[target_name].phony then
+            return math.huge
+        else
+            return fs.lastModified(target_path)
+        end
+    end
+
     if traversed_targets[target] then
+        -- Cyclic dependency found
         return -1
     end
-    traversed_targets[target] = true
     if target.update_time ~= -1 then
         -- Do not run a given target more than once
         return target.update_time
     end
+    traversed_targets[target] = true
 
-    -- Find greatest modification timestamp of all dependency files
-    local depend_update_time = -1
-    for _,dependency in ipairs(target.dependencies) do
-        local update_time = run_target(get_target(dependency), false)
-        if update_time == -1 then
-            print("bake: Circular " .. target.name .. " <- " .. get_target(dependency).name .. " dependency dropped.")
-        end
-        depend_update_time = math.max(depend_update_time, update_time)
-    end
-
-    -- Finished dependencies for this target, remove the traversed_targets entry
-    -- so we don't mark it as a cycle in another target
-    traversed_targets[target] = nil
-
-    local target_path = shell.resolve(target.name)
-    if fs.exists(target_path) then
+    if fs.exists(target_path) and not targets_metadata[target_name].phony then
         target.update_time = fs.lastModified(target_path)
     else
         target.update_time = math.huge
     end
 
+    -- Create a space-separated list of each dependency, and another for just
+    -- the dependencies that changed for the target
+    local all_dependencies = ""
+    local updated_dependencies = ""
+    for _,dependency in ipairs(target.dependencies) do
+        local update_time = run_target(dependency, false)
+        if update_time == -1 then
+            print("bake: Circular " .. target_name .. " <- " .. dependency .. " dependency dropped.")
+        end
+        all_dependencies = all_dependencies .. dependency .. " "
+        if update_time == math.huge or target.update_time == math.huge or update_time > target.update_time then
+            updated_dependencies = updated_dependencies .. dependency .. " "
+        end
+    end
+    all_dependencies = all_dependencies:sub(1, -2)
+    updated_dependencies = updated_dependencies:sub(1, -2)
+
+    -- Finished dependencies for this target, remove the traversed_targets entry
+    -- so we don't mark it as a cycle in another target
+    traversed_targets[target] = nil
+
     -- Only run the target if no corresponding file found, or dependency updated
-    if target.update_time == math.huge or depend_update_time > target.update_time then
+    if target.update_time == math.huge or #updated_dependencies > 0 then
+        local automaticVars = {
+            ["@"] = target_name,
+            ["?"] = updated_dependencies,
+            ["^"] = all_dependencies
+        }
         for _,command in ipairs(target.commands) do
-            run(command)
+            run(command:gsub("%$([@?^])", automaticVars))
         end
         return math.huge
     elseif is_first then
-        print("bake: \'" .. target.name .. "\' is up to date.")
+        print("bake: \'" .. target_name .. "\' is up to date.")
     end
 
     return target.update_time
 end
 
-if #targets == 0 then
+if #targets_ordering == 0 then
     io.stderr:write("bake: No targets defined\n")
     os.exit(1)
 else
     if args.target then
-        run_target(get_target(args.target), true)
+        run_target(args.target, true)
     else
-        run_target(targets[1], true)
+        run_target(targets_ordering[1], true)
     end
 end
